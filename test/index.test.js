0d66822d26e1c2de92cc6fcb1c4d3c55d07ffd9d
balmacefa <fabianbalmaceda2@gmail.com>
2020-12-29 23:06:55 -0600

    lib refactor and test

--- test/index.test.js
+++ test/index.test.js
@@ -1,36 +1,89 @@
+/* eslint-disable no-unused-vars */
 "use strict";
 
 var fs = require("fs");
 var path = require("path");
 var mdMetadataParser = require("../lib");
 
-const filePath = path.join(__dirname, "/md_templates/01_template.md");
+const mdStandardFilePath = path.join(__dirname, "/templates/01_template.md");
+const jsonStandardFilePath = path.join(
+  __dirname,
+  "/templates/01_template.json"
+);
 
-const startFormatNode = "<!-- __NODE_START__  -->";
+const mdCustomFilePath = path.join(__dirname, "/templates/02_template.md");
+const jsonCustomFilePath = path.join(__dirname, "/templates/02_template.json");
+
+const startFormatNode = "<!-- __NODE_BEGIN__  -->";
 const endFormatNode = "<!-- __NODE_END__  -->";
 
-const startFormatMetadata = "<!-- __meta_start__";
-const endFormatMetadata = "__meta_end__ -->";
+const startFormatMetadata = "<!-- __info_start__";
+const endFormatMetadata = "__info_end__ -->";
 
-const startFormatContent = "<!-- __content_start__ -->";
-const endFormatContent = "<!-- __content_end__ -->";
+const startFormatContent = "<!-- __render_start__ -->";
+const endFormatContent = "<!-- __render_end__ -->";
 
-const startFormatChildren = "<!-- __children_start__ -->";
-const endFormatChildren = "<!-- __children_end__ -->";
+const startFormatChildren = "<!-- __nodes_start__ -->";
+const endFormatChildren = "<!-- __nodes_end__ -->";
 
-fs.readFile("build.md", "utf8", function(err, data) {
-  if (err) {
-    console.error(err);
-  }
+describe("Standard MD parser e2e", () => {
+  test("parse Md to Json", () => {
+    var mdData = fs.readFileSync(mdStandardFilePath, "utf8");
+    var jsonData = fs.readFileSync(jsonStandardFilePath, "utf8");
 
-  var resultTree = mdMetadataParser.parseMd2Json({ mdData: data });
+    var resultTree = mdMetadataParser.parseMd2Json({ mdData });
+    expect(resultTree).toEqual(JSON.parse(jsonData));
+  });
+  test("parse Json to MD", () => {
+    var mdData = fs.readFileSync(mdStandardFilePath, "utf8");
+    var resultTree = mdMetadataParser.parseMd2Json({ mdData });
+    var outputMD = mdMetadataParser.parseJson2Md({ rootNode: resultTree });
+    expect(outputMD).toBe(mdData);
+  });
+});
 
-  // Console.log(JSON.stringify(resultTree, null, 4));
-  var outputMD = mdMetadataParser.parseJson2Md({ rootNode: resultTree });
-  console.log(outputMD);
-  // Fs.writeFile("build.md", outputMD, "utf8", function(err, data) {
-  //   if (err) {
-  //     console.error(err);
-  //   }
-  // });
+describe("Custom MD parser e2e", () => {
+  test("parse Md to Json", () => {
+    var mdData = fs.readFileSync(mdCustomFilePath, "utf8");
+    var jsonData = fs.readFileSync(jsonCustomFilePath, "utf8");
+
+    var resultTree = mdMetadataParser.parseMd2Json({
+      mdData,
+      startFormatNode,
+      endFormatNode,
+      startFormatMetadata,
+      endFormatMetadata,
+      startFormatContent,
+      endFormatContent,
+      startFormatChildren,
+      endFormatChildren
+    });
+    expect(resultTree).toEqual(JSON.parse(jsonData));
+  });
+  test("parse Json to MD", () => {
+    var mdData = fs.readFileSync(mdCustomFilePath, "utf8");
+    var resultTree = mdMetadataParser.parseMd2Json({
+      mdData,
+      startFormatNode,
+      endFormatNode,
+      startFormatMetadata,
+      endFormatMetadata,
+      startFormatContent,
+      endFormatContent,
+      startFormatChildren,
+      endFormatChildren
+    });
+    var outputMD = mdMetadataParser.parseJson2Md({
+      rootNode: resultTree,
+      startFormatNode,
+      endFormatNode,
+      startFormatMetadata,
+      endFormatMetadata,
+      startFormatContent,
+      endFormatContent,
+      startFormatChildren,
+      endFormatChildren
+    });
+    expect(outputMD).toBe(mdData);
+  });
 });
---
+++ test/templates/01_template.md
@@ -0,0 +1,99 @@
+<!-- __NODE_START__  -->
+<!-- __meta_start__
+id: 1
+description: 1111
+date: 2020-12-23T22:38:58.000Z
+
+__meta_end__ -->
+<!-- __content_start__ -->
+# Content 1
+Content 1
+<!-- __content_end__ -->
+<!-- __children_start__ -->
+<!-- __NODE_START__  -->
+<!-- __meta_start__
+id: 01a
+description: 1a
+
+__meta_end__ -->
+<!-- __content_start__ -->
+Content 1a
+<!-- __content_end__ -->
+
+<!-- __NODE_END__  -->
+
+<!-- __children_end__ -->
+
+<!-- __NODE_END__  -->
+<!-- __NODE_START__  -->
+<!-- __meta_start__
+id: 2
+description: 2222
+
+__meta_end__ -->
+<!-- __content_start__ -->
+## Content 2
+Content 2
+<!-- __content_end__ -->
+
+<!-- __NODE_END__  -->
+<!-- __NODE_START__  -->
+<!-- __meta_start__
+id: 3
+description: "3333"
+json:
+  - _id: 5fe3ca15641eec9d7aee7aba
+    index: 0
+    guid: 535b42df-bc19-4854-bdb2-612110cee8a9
+    isActive: true
+    balance: $2,497.92
+    picture: http://placehold.it/32x32
+    age: 33
+    eyeColor: blue
+    name:
+      first: Graves
+      last: Bolton
+    company: OLUCORE
+    phone: +1 (997) 477-2790
+    address: 433 Riverdale Avenue, Hebron, District Of Columbia, 3624
+    about: Nostrud aliquip enim reprehenderit magna consequat. Dolore nostrud dolore
+      tempor laboris ullamco sint est veniam reprehenderit ut reprehenderit
+      ipsum cillum officia. Exercitation deserunt aliqua ex reprehenderit enim
+      dolore. Et reprehenderit qui consectetur ad tempor consequat dolor magna
+      magna ullamco tempor proident irure. Eu sint ex exercitation deserunt
+      dolor laboris veniam enim mollit sint.
+    registered: Wednesday, December 6, 2017 3:10 PM
+    latitude: "47.248053"
+    longitude: "85.65806"
+    tags:
+      - tempor
+      - dolore
+      - occaecat
+      - veniam
+      - non
+    range:
+      - 0
+      - 1
+      - 2
+      - 3
+      - 4
+      - 5
+      - 6
+      - 7
+      - 8
+      - 9
+    friends:
+      - id: 0
+        name: Traci Carson
+      - id: 1
+        name: Davis Lancaster
+      - id: 2
+        name: Carolina Durham
+
+__meta_end__ -->
+<!-- __content_start__ -->
+## Content 3
+Content 3
+<!-- __content_end__ -->
+
+<!-- __NODE_END__  -->
